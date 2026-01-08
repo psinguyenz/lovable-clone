367391e1100dddd89335ee686d3326f8578b5fd4
Hoahong2002 <psinguyenz@gmail.com>
2026-01-08 22:47:23 +0700

    06: e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-siz-test-4");
+      return sandbox.sandboxId;
+    });
+
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: "code-agent",
@@ -16,9 +23,13 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet: ${event.data.value}!`,
     );
 
-    console.log(output);
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000) // create a host under port 3000
+      return `https://${host}`;
+    })
 
-    return { output };
+    return { output, sandboxUrl };
   },
 );
 

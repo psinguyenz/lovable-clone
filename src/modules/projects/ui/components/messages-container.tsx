54d4b337518c8270c9cea41a6e8c813e6a665ac5
Hoahong2002 <psinguyenz@gmail.com>
2026-01-10 22:49:23 +0700

    10: messages ui

---
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -0,0 +1,63 @@
+// no need the "use-client" since project-view already has it so its children will as well
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { MessageCard } from "./message-card";
+import { MessageForm } from "./message-form";
+import { useEffect, useRef } from "react";
+
+interface Props {
+    projectId: string;
+};
+
+export const MessagesContainer = ({ projectId }: Props) => {
+    const bottomRef = useRef<HTMLDivElement>(null);
+    const trpc = useTRPC();
+
+    // the useSuspenseQuery if being used in a deeper component (like messages-container) then the page will load faster
+    // It's not really faster tho but visually faster
+    const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
+        projectId: projectId,
+    }));
+
+    useEffect(() => {
+        const lastAssistantMessage = messages.findLast(
+            (message) => message.role === "ASSISTANT", // load the last message the assistant sent
+        );
+
+        if (lastAssistantMessage) {
+            // TODO SET ACTIVE FRAGMENT
+        }
+    }, [messages]);
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView();
+    }, [messages.length]);
+
+    return (
+        <div className="flex flex-col flex-1 min-h-0">
+            <div className="flex-1 min-h-0 overflow-y-auto">
+                <div className="pt-2 pr1">
+                    {messages.map((message) => (
+                        <MessageCard 
+                            key={message.id}
+                            content={message.content}
+                            role={message.role}
+                            fragment={message.fragment}
+                            createdAt={message.createdAt}
+                            isActiveFragment={false}
+                            onFragmentClick={() => {}}
+                            type={message.type}
+                        />
+                    ))}
+                    {/* auto scroll down to the last message */}
+                    <div ref={bottomRef} /> 
+                </div>
+            </div>
+            <div className="relative p-3 pt-1">
+                {/* this div create the fade out transition */}
+                <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background/70 pointer-events-none" /> 
+                <MessageForm projectId={projectId} />
+            </div>
+        </div>
+    );
+};

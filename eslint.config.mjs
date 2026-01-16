d4989152e8cc79878fafd1bbfbe50495f847c516
Hoahong2002 <psinguyenz@gmail.com>
2026-01-16 21:32:34 +0700

    20: deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignore: ["**/generated/*"] // still linting the rest of the project
+  },
 ];
 
 export default eslintConfig;
